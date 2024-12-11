4b8c85847608c3d105c4e4d3f1c26533da8cd6f9
Lucas Oliveira Santiago <lucas.santiago@fama.local>
2024-12-11 13:36:54 -0300

    integração com api, acrescimo da tela principal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import {View, Text} from "react-native"
+import { router } from "expo-router"
 
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 import { Welcome } from "@/components/welcome"
 
 
@@ -9,13 +11,13 @@ export default function Index(){
     
     return (
         <View
-        style={{
-            flex:1,
-            padding: 40,
-            gap: 40,
-        }}>
+        style={{flex:1,padding: 40,gap: 40,}}>
             <Welcome />
             <Steps />
+
+            <Button onPress={() => router.navigate("/home")}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
