c3ca21669faf7a8888945eaf52c302e3a348917e
Lucas Oliveira Santiago <lucas.santiago@fama.local>
2024-12-12 10:25:18 -0300

    adicionado uma melhor customização da tela home, scrolando para cima e baixo dimensionando tela dinamica

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,34 +1,61 @@
 import { useEffect, useState } from "react"
-import { View, Text, Alert } from "react-native";
+import { View, Alert } from "react-native"
 
 import { api } from "@/services/api"
 
-import { Categories, CategoriesProps } from "@/components/categories";
+import { Places } from "@/components/places"
+import { PlaceProps } from "@/components/place"
+import { Categories, CategoriesProps } from "@/components/categories"
+
+type MarketsProps = PlaceProps
 
 export default function Home() {
-    const [categories, setCategories] = useState<CategoriesProps>([])
-    const [category, setCategory] = useState("")
+  const [categories, setCategories] = useState<CategoriesProps>([])
+  const [category, setCategory] = useState("")
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
-
-    async function fetchCategories(){
-        try {
-            const { data } = await api.get("/categories")
-            setCategories(data)
-            setCategory(data[0].id)
-
-        } catch (error) {
-            console.log(error)
-            Alert.alert("Categorias", "Não fi possível carregar as categorias.")
-        }
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      setCategories(data)
+      setCategory(data[0].id)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.")
     }
+  }
 
-    useEffect(() => {
-        fetchCategories()
-    }, [])
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return
+      }
 
-    return (
-        <View style={ { flex:1 } }>
-            <Categories data={categories} onSelect={setCategory} selected={category}/>
-        </View>
-    )
+      const { data } = await api.get("/markets/category/" + category)
+      setMarkets(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Locais", "Não foi possível carregar os locais.")
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  useEffect(() => {
+    fetchMarkets()
+  }, [category])
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
+
+      <Places data={markets} />
+    </View>
+  )
 }
