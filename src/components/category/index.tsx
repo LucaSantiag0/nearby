49a5f89f9fd5be213a4cb0e5262d6d9dda53898f
Lucas Oliveira Santiago <lucas.santiago@fama.local>
2025-01-08 08:55:23 -0300

    finalizacao do projeto

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -4,19 +4,22 @@ import { s } from "./styles"
 import { colors } from "@/styles/theme"
 import { categoriesIcons } from "@/utils/categories-icons"
 
-type Props =  PressableProps & {
-    iconId: string
-    isSelected?: boolean
-    name: string
+type Props = PressableProps & {
+  name: string
+  iconId: string
+  isSelected?: boolean
 }
 
-export function Category ({name, iconId, isSelected =false, ...rest}: Props) {
-    const Icon = categoriesIcons[iconId]
+export function Category({ name, iconId, isSelected = false, ...rest }: Props) {
+  const Icon = categoriesIcons[iconId]
 
-    return (
-        <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
-            <Icon size={16}  color={colors.gray[isSelected ? 100 : 400 ]}/>
-            <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
-        </Pressable>
-    )
+  return (
+    <Pressable
+      style={[s.container, isSelected && s.containerSelected]}
+      {...rest}
+    >
+      <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
+      <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
+    </Pressable>
+  )
 }
